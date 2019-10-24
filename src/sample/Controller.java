89d528799b3ac0e626b6a52bf7a15ceaf0eec017
Michał Gradkowski <migradyt@gmail.com>
2019-10-24 12:57:48 +0200

    testy

---
+++ src/sample/Controller.java
@@ -0,0 +1,26 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+import java.sql.SQLOutput;
+
+import static javafx.event.ActionEvent.ACTION;
+
+public class Controller {
+    private Button button;
+
+    public void close() {
+        System.exit(0);
+    }
+    public void mobbyn(){
+        System.out.println("Siema");
+    }
+
+    void initialize() {
+
+    }
+}
+
