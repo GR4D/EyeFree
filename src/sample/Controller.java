feff7f959333610bc9e32a8c991735bb8528cb67
Michał Gradkowski <migradyt@gmail.com>
2019-11-01 22:51:08 +0100

    words

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -40,111 +41,93 @@ public class Controller {
     private int errorWords = 0;
     private int letterGood = 0;
     private int letterError = 0;
-
-    //String[] newContent = "";
-
-//    @FXML
-//    public static String readAllBytes(String filePath){
-//        String content = new String();
-//        try{
-//            content = new String (Files.readAllBytes(Paths.get(filePath)));
-//        }catch(IOException e){
-//            e.printStackTrace();
-//        }
-//        return content;
-//    }
+    private boolean isBackspaceKey = false;
 
     @FXML
-    public void keyPressed(KeyEvent e){
+    public void isBackspace(KeyEvent e){
+        if(e.getCode() == KeyCode.BACK_SPACE){
+            //System.out.println("Backspace chuj");
+            isBackspaceKey = true;
+            if(letterCounter != 0){
+                letterCounter--;
+            }
+        }else{
+            //System.out.println("Nie jest to backsapce");
+            isBackspaceKey = false;
+        }
+    }
 
-       if( Character.isSpaceChar(e.getCharacter().charAt(0)) ){    //jezeli spacja to koniec słowa i sprawdzenie poprawnosci
-//           System.out.println("Spacja");
-//           System.out.println("Tekst do wpisania: "+textToWriteLabel.getText());
-//           System.out.println("tekst z pola: "+textInputField.getText());
-           textInputField.deletePreviousChar();
+    @FXML
+    public void keyPressed(KeyEvent e) {
+        if (!isBackspaceKey) {
+            if (isSpaceChar(e.getCharacter().charAt(0))) {    //jezeli spacja to koniec słowa i sprawdzenie poprawnosci
 
+                textInputField.deletePreviousChar();
 
-           if(wordCounter  != newContent.length) {
+                if (wordCounter != newContent.length) {
 
-
-               if (textInputField.getText().equals(newContent[wordCounter])) {
-                   System.out.println("zgadza sie");
-                   goodWords++;
-                   wordCounter++;
-               } else {
-                   System.out.println("nie zgadza sie");
-                   errorWords++;
-                   wordCounter++;
+                    if (textInputField.getText().equals(newContent[wordCounter])) {
+                        System.out.println("zgadza sie");
+                        goodWords++;
+                        wordCounter++;
+                    } else {
+                        System.out.println("nie zgadza sie");
+                        errorWords++;
+                        wordCounter++;
 //               currentLetterWord = newContent[wordCounter].toCharArray();
-               }
+                    }
+                    //wordCounter++;
+                    textInputField.clear();
+                    letterCounter = 0;
+                    if (wordCounter != newContent.length) {
+                        currentLetterWord = newContent[wordCounter].toCharArray();
+                    }
 
-               //wordCounter++;
-               textInputField.clear();
-               letterCounter = 0;
-               if (wordCounter != newContent.length) {
-                   currentLetterWord = newContent[wordCounter].toCharArray();
-               }
+                }
+                //System.out.println(Character.isSpaceChar(e.getCharacter().charAt(0))); //Wykrywanie spacji
 
-           }
-                                   //System.out.println(Character.isSpaceChar(e.getCharacter().charAt(0))); //Wykrywanie spacji
+                if (wordCounter == newContent.length) {
+                    testLabel.setText("Koniec wpisywania! Poprawnych słów: " + goodWords + " Błędnych słów: " + errorWords +  " Popełnionych błędów: " + letterError);
+                    System.out.println("Koniec dddddddddddddddddddddd");
+                    System.out.println("Poprawnych słów: " + goodWords);
+                    System.out.println("Błędnych słów: " + errorWords);
+                    System.out.println("Popełnionych błędów: " + letterError);
+                }
+            }/////////////////////////////////////////////////////////////////////////////
+            if (wordCounter != newContent.length & letterCounter < currentLetterWord.length) {
+                System.out.println("litera: " + currentLetterWord[letterCounter]);
 
-           if(wordCounter  == newContent.length){
-               testLabel.setText("Koniec wpisywania! Poprawnych słów: " +goodWords+ " Błędnych słów: "+errorWords+ " Poprawnych liter: " +letterGood+ " Błędnych liter: "+letterError);
-               System.out.println("Koniec dddddddddddddddddddddd");
-               System.out.println("Poprawnych słów: " +goodWords);
-               System.out.println("Błędnych słów: " +errorWords);
-               System.out.println("Poprawnych liter: " +letterGood);
-               System.out.println("Błędnych liter: " +letterError);
-           }
-       }/////////////////////////////////////////////////////////////////////////////
-        if(wordCounter != newContent.length & letterCounter < currentLetterWord.length){
-            System.out.println("litera: " + currentLetterWord[letterCounter]);
-
-            if(e.getCharacter().charAt(0) == currentLetterWord[letterCounter] & !Character.isSpaceChar(e.getCharacter().charAt(0))){
-                System.out.println("ruwna sie ta litera");
-                letterGood++;
-            }else if(e.getCharacter().charAt(0) != currentLetterWord[letterCounter] & !Character.isSpaceChar(e.getCharacter().charAt(0))){
-                System.out.println("nieruwna sie ta litera");
-                letterError++;
+                if (e.getCharacter().charAt(0) == currentLetterWord[letterCounter] & !isSpaceChar(e.getCharacter().charAt(0))) {
+                    System.out.println("ruwna sie ta litera");
+                    letterGood++;
+                } else if (e.getCharacter().charAt(0) != currentLetterWord[letterCounter] & !isSpaceChar(e.getCharacter().charAt(0))) {
+                    System.out.println("nieruwna sie ta litera");
+                    letterError++;
+                }
+                if (!isSpaceChar(e.getCharacter().charAt(0)))
+                    letterCounter++;
             }
 
-            if( !Character.isSpaceChar(e.getCharacter().charAt(0)) )
-                letterCounter++;
         }
-
     }
 
     public void loadTekst()throws IOException{
         wordCounter = 0;
-        String content = new String();
 
         try{
-            content = new String (Files.readAllBytes(Paths.get("src/sample/slowa.txt")));
+            String content = new String(Files.readAllBytes(Paths.get("src/sample/slowa.txt")));
+            newContent = content.split(" ");
+            currentLetterWord = newContent[wordCounter].toCharArray();
+            textToWriteLabel.setText(content);
+            System.out.println("Poprawnie załadowano plik.");
         }
         catch(IOException e){
-            e.printStackTrace();
+            //e.printStackTrace();
+            System.out.println("Błąd ładowania pliku");
         }
-
-        newContent = content.split(" ");
-        currentLetterWord = newContent[wordCounter].toCharArray();
-        System.out.println("litera: " + currentLetterWord[0]);
-        textToWriteLabel.setText(content);
     }
 
     public void setTeksto(ActionEvent event)throws IOException{
-//        wordCounter = 0;
-//        String content = new String();
-//        try{
-//            content = new String (Files.readAllBytes(Paths.get("src/sample/slowa.txt")));
-//        }
-//        catch(IOException e){
-//            e.printStackTrace();
-//        }
-//
-//        newContent = content.split(" ");
-//        currentLetterWord = newContent[wordCounter].toCharArray();
-//        System.out.println("litera: " + currentLetterWord[0]);
-//        textToWriteLabel.setText(content);
         loadTekst();
     }
 
